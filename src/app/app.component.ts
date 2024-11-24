b955b3dd2d964a08d2ad757615b3e528cbfdc300
MhdYoussef <mohammad.youssef963@gmail.com>
2024-11-24 15:13:49 +0300

    Update to Angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { Component } from '@angular/core';
  import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-   imports: 
-          [
-            //RouterOutlet,
-           NavBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        //RouterOutlet,
+        NavBarComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ecommerce Site';
